f193fbbb5c2ea67c1b879769a835d796699bb2f4
Cody Bruno <cody@turbot.com>
2021-05-20 15:30:36 -0400

    Fix query for CIS v1.2.0 1.6 control

--- query/iam/iam_user_not_assigned_service_account_user_role_project_level.sql
+++ query/iam/iam_user_not_assigned_service_account_user_role_project_level.sql
@@ -1,21 +1,28 @@
+with unapproved_bindings as (
+  select
+    project,
+    p,
+    entity
+  from
+    gcp_iam_policy,
+    jsonb_array_elements(bindings) as p,
+    jsonb_array_elements_text(p -> 'members') as entity
+  where
+    p ->> 'role' in ('roles/iam.serviceAccountTokenCreator','roles/iam.serviceAccountUser')
+    and entity not like '%iam.gserviceaccount.com'
+)
 select
-  -- Required Columns
-  split_part(string_agg(project, ', '), ',', 1 ) as resource,
+  p.project as resource,
   case
-    when count(*) > 1 then 'alarm'
+    when entity is not null then 'alarm'
     else 'ok'
   end status,
   case
-    when count(*) > 1
-      then 'IAM users associated with ''iam.serviceAccountTokenCreator'' or ''iam.serviceAccountUser'' role.'
-    else 'No IAM users associated with ''iam.serviceAccountTokenCreator'' or ''iam.serviceAccountUser'' role.'
+    when entity is not null
+      then 'IAM users associated with iam.serviceAccountTokenCreator or iam.serviceAccountUser role.'
+    else 'No IAM users associated with iam.serviceAccountTokenCreator or iam.serviceAccountUser role.'
   end reason,
-  -- Additional Dimensions
-  split_part(string_agg(project, ', '), ',', 1 )
+  p.project
 from
-  gcp_iam_policy,
-  jsonb_array_elements(bindings) as p,
-  jsonb_array_elements_text(p -> 'members') as entity
-where
-  p ->> 'role' in ('roles/iam.serviceAccountTokenCreator','roles/iam.serviceAccountUser')
-  and entity not like '%iam.gserviceaccount.com';
+  gcp_iam_policy as p
+  left join unapproved_bindings as b on p.project = b.project
