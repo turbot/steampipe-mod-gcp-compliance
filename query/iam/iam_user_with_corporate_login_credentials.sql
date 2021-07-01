97a7c6d103104da9e47d4ec453b902e473c68b45
Subhajit Kumar Mondal <subhajit@turbot.com>
2021-07-01 16:33:07 +0530

    Add check for CIS v1.2.0 Section 1.1. Closes #37

---
+++ query/iam/iam_user_with_corporate_login_credentials.sql
@@ -0,0 +1,27 @@
+with user_with_acces as (
+  select
+    distinct split_part(m, ':', 2) as member,
+    project
+  from
+    gcp_iam_policy,
+    jsonb_array_elements(bindings) as b,
+    jsonb_array_elements_text(b -> 'members') as m
+  where
+    m like 'user:%'
+)
+select
+  -- Required Columns
+  a.member as resource,
+  case
+    when org.display_name is null then 'alarm'
+    else 'ok'
+  end as status,
+  case
+    when org.display_name is null then a.member || ' having non-corporate login credentials.'
+    else a.member || ' having corporate login credentials.'
+  end as reason,
+  -- Additional Dimensions
+  a.project
+from
+  user_with_acces as a
+  left join gcp_organization as org on split_part(a.member, '@', 2) = org.display_name;
